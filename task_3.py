6bcbe21d12831104af72939f517ae7ba720c2f1e
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-06-12 21:25:02 +0300

    add third task

--- task_3.py
+++ task_3.py
@@ -1,6 +1,9 @@
-digit_sum = 0
-num = 6859
-while num:
-    digit_sum += num % 10
-    num = num // 10
-print(digit_sum)
+percent = int(input('Введите количество процентов от 0 до 20: '))
+if percent == 0 or 21 > percent > 4:
+    print(percent, 'процентов')
+elif percent == 1:
+    print(percent, 'процент')
+elif percent > 20:
+    print('Не верный ввод')
+else:
+    print(percent, 'процента')
