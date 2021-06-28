1f9ad26d428e7faf072cc23b680b9bed791ee804
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-06-28 18:34:45 +0300

    edit taks 5

--- task_5.py
+++ task_5.py
@@ -1,13 +1,14 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-# result = [i for i in src if src.count(i) == 1] так делать нельзя
-# не знаю насколько дешевле использовать remove, но мне кажется всяко дешевле, чем count
+# result = [i for i in src if src.count(i) == 1]  так делать нельзя - дорого)
+
 set_1 = set()
-result = []
+set_2 = set()
 for i in src:
     if i in set_1:
-        if i in result:
-            result.remove(i)
+        if i in set_2:
+            set_2.discard(i)
     else:
         set_1.add(i)
-        result.append(i)
+        set_2.add(i)
+result = [el for el in src if el in set_2]
 print(result)
