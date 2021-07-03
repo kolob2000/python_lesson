912192b6168c5d5e8ac531268fecdea16579a221
kolob2000 <49161276+kolob2000@users.noreply.github.com>
2021-07-03 12:29:52 +0300

    add 4 5 task

--- task_3_4_5/task_4_5.py
+++ task_3_4_5/task_4_5.py
@@ -1,8 +1,10 @@
 import os
+import sys
 
-def users_hobby_function(users_file: str, hobby_file: str) -> dict:
-    if os.path.exists('users_hobby.txt'):
-        os.remove('users_hobby.txt')
+
+def users_hobby_function(users_file: str, hobby_file: str) -> str:
+    if os.path.exists(sys.argv[3]):
+        os.remove(sys.argv[3])
     with open(users_file, 'r', encoding='utf-8') as users:
         with open(hobby_file, 'r', encoding='utf-8') as hobby:
             while True:
@@ -14,8 +16,9 @@ def users_hobby_function(users_file: str, hobby_file: str) -> dict:
                     break
                 if not temp_users:
                     return 1
-                with open('users_hobby.txt', 'a', encoding='utf-8') as f:
+                with open(sys.argv[3], 'a', encoding='utf-8') as f:
                     f.writelines(temp_users + ': ' + str(temp_hobby))
-                    
+    return print('ok')
 
-users_hobby_function('user.csv', 'hobby.csv')
+
+users_hobby_function(sys.argv[1], sys.argv[2])
