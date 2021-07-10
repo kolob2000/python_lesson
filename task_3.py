ca151a1ad1cd4f49581e73f8c22efd31e1957757
Nick <k.kolabis@mail.ru>
2021-07-10 15:45:13 +0300

    add task_3

---
+++ task_3.py
@@ -0,0 +1,19 @@
+def type_log(func):
+    def log_wrapper(*args, **kwargs):
+        print(f'{type(func(*args, **kwargs))} {func.__name__}(', end='')
+        for i in args:
+            print(f'{i}: {type(i)}, ', end='')
+        for j, k in kwargs.items():
+            print(f'{j}={k}: {type(k)},', end='')
+        print(')')
+        return func(*args, **kwargs)
+
+    return log_wrapper
+
+
+@type_log
+def add(a=1, b=7):
+    return a + b
+
+
+print(add(7.0, b=24))
